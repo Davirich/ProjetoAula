d7af4b28009fc3c8b275337df1cb0a4acb07b0bd
DaviDieterich <davi.edu.dieterich@gmail.com>
2024-08-02 19:01:12 -0300

    Text Menu, pratica02

---
+++ MountainShooter/pythonProject/code/Level.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.entity_list = None
+        self.name = None
+
+    def run(self, ):
+        pass
+
+    def run(self, ):
+        pass
+
+    def run(self, ):
+        pass
+
