a7aeb7491891bf97fb70d4d3cb8592092c05610d
DaviDieterich <davi.edu.dieterich@gmail.com>
2024-08-07 22:50:15 -0300

    Aula sobre criação e movimento de entidades

--- MountainShooter/pythonProject/code/Menu.py
+++ MountainShooter/pythonProject/code/Menu.py
@@ -16,8 +16,9 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
-        # pygame.mixer_music.load('./asset/Menu.mp3')
-        # pygame.mixer_music.play(-1)
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        pygame.mixer_music.set_volume(0.15)
         menu_option = 0
         while True:
             # Desenhar na tela
@@ -53,6 +54,6 @@ class Menu:
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, True, text_color)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
