0c2e529930eb525eb5c5d048d497afa37b0d26da
DaviDieterich <davi.edu.dieterich@gmail.com>
2024-08-10 01:22:08 -0300

    Aula de criacao de pontuacoes

--- MountainShooter/pythonProject/code/Const.py
+++ MountainShooter/pythonProject/code/Const.py
@@ -1,9 +1,11 @@
 # C
 import pygame
 
-COLOR_ORANGE = (255, 128, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YELLOW = (255, 255, 128)
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 128)
+C_GREEN = (0, 127, 0)
+C_CYAN = (0, 127, 127)
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
@@ -25,13 +27,29 @@ ENTITY_SPEED = {'Level1Bg0': 0,
                 'Level1Bg6': 6,
                 'Player1': 3,
                 'Player1Shot': 4,
-                'Player2': 3,
+                'Player2': 2,
                 'Player2Shot': 3,
                 'Enemy1': 2,
                 'Enemy1Shot': 3,
                 'Enemy2': 1,
                 'Enemy2Shot': 3
                 }
+ENTITY_DAMAGE = {'Level1Bg0': 0,
+                 'Level1Bg1': 0,
+                 'Level1Bg2': 0,
+                 'Level1Bg3': 0,
+                 'Level1Bg4': 0,
+                 'Level1Bg5': 0,
+                 'Level1Bg6': 0,
+                 'Player1': 1,
+                 'Player1Shot': 15,
+                 'Player2': 1,
+                 'Player2Shot': 30,
+                 'Enemy1': 1,
+                 'Enemy1Shot': 10,
+                 'Enemy2': 1,
+                 'Enemy2Shot': 20
+                 }
 
 ENTITY_HEALTH = {'Level1Bg0': 999,
                  'Level1Bg1': 999,
@@ -40,15 +58,32 @@ ENTITY_HEALTH = {'Level1Bg0': 999,
                  'Level1Bg4': 999,
                  'Level1Bg5': 999,
                  'Level1Bg6': 999,
-                 'Player1': 300,
+                 'Player1': 100,
                  'Player1Shot': 1,
-                 'Player2': 300,
+                 'Player2': 150,
                  'Player2Shot': 1,
-                 'Enemy1': 1,
-                 'Enemy1Shot': 200,
-                 'Enemy2': 200,
+                 'Enemy1': 30,
+                 'Enemy1Shot': 1,
+                 'Enemy2': 60,
                  'Enemy2Shot': 1
                  }
+
+ENTITY_SCORE = {'Level1Bg0': 0,
+                'Level1Bg1': 0,
+                'Level1Bg2': 0,
+                'Level1Bg3': 0,
+                'Level1Bg4': 0,
+                'Level1Bg5': 0,
+                'Level1Bg6': 0,
+                'Player1': 0,
+                'Player1Shot': 0,
+                'Player2': 0,
+                'Player2Shot': 0,
+                'Enemy1': 100,
+                'Enemy1Shot': 0,
+                'Enemy2': 125,
+                'Enemy2Shot': 0
+                }
 
 ENTITY_SHOT_DELAY = {'Player1': 20,
                      'Player2': 40,
--- MountainShooter/pythonProject/code/Level.py
+++ MountainShooter/pythonProject/code/Level.py
@@ -7,7 +7,7 @@ import pygame.display
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import COLOR_WHITE, MENU_OPTION, EVENTO_ENEMY
+from code.Const import C_WHITE, MENU_OPTION, EVENTO_ENEMY, WIN_HEIGHT, C_GREEN, C_CYAN
 from code.Enemy import Enemy
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
@@ -43,9 +43,13 @@ class Level:
                     shoot = ent.shoot()
                     if shoot is not None:
                         self.entity_list.append(shoot)
+                if ent.name == 'Player1':
+                    self.level_text(14, f'Player1 - Health:  {ent.health} | Score: {ent.score}', C_GREEN, (10, 25))
+                if ent.name == 'Player2':
+                    self.level_text(14, f'Player1 - Health:  {ent.health} | Score: {ent.score}', C_CYAN, (10, 45))
             # texto a ser printado na tela
-            self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_WHITE, (10, 10))
-            self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (20, 10))
+            self.level_text(14, f'fps: {clock.get_fps():.0f}', C_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entidades:{len(self.entity_list)}', C_WHITE, (10, WIN_HEIGHT - 20))
             # Atualizar tela
             pygame.display.flip()
             # VERIFICAR RELACIONAMENTOS DE ENTIDADES
@@ -65,5 +69,5 @@ class Level:
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
-        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[0])
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
         self.window.blit(source=text_surf, dest=text_rect)
